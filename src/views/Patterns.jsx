68ed457f4a3802a9c85a15d0a590870edc33d2a8
Tomek <tomekz@vp.pl>
2021-02-18 18:44:40 +0100

    Testing render props

--- src/views/Patterns.jsx
+++ src/views/Patterns.jsx
@@ -1,8 +1,17 @@
+/* eslint-disable */
 import React from 'react';
+
+const MyComponent = ({ children }) => (
+  <div>
+    <p>Hello World!</p>
+    {children}
+  </div>
+);
 
 const Patterns = () => (
   <div>
     <h2 className='title is-3'>Patterns</h2>
+    <MyComponent>Testing</MyComponent>
   </div>
 );
 
