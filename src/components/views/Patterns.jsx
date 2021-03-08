5a3bd7affb24ddd6aff6ea266c692b68c126b1b7
Tomek <tomekz@vp.pl>
2021-03-08 15:47:18 +0100

    Add Downshift dropdown menu to the Patterns view

--- src/components/views/Patterns.jsx
+++ src/components/views/Patterns.jsx
@@ -1,18 +1,39 @@
-/* eslint-disable */
 import React from 'react';
+import Downshift from 'downshift';
+import cx from 'classnames';
 
-const MyComponent = ({ children }) => (
-  <div>
-    <p>Hello World!</p>
-    {children('Roman')}
-  </div>
-);
-
+const items = [
+  { value: 'apple' },
+  { value: 'pear' },
+  { value: 'orange' },
+  { value: 'grape' },
+  { value: 'banana' },
+];
 const Patterns = () => (
-  <div>
-    <h2 className='title is-3'>Patterns</h2>
-    <MyComponent>{(arg) => arg}</MyComponent>
-  </div>
+  <Downshift>
+    {({ isOpen, inputValue, getInputProps }) => (
+      <div className={cx('dropdown', { 'is-active': isOpen })}>
+        <input
+          className='input'
+          type='text'
+          placeholder='This is an input...'
+          {...getInputProps()}
+        />
+
+        <div className='dropdown-menu'>
+          <div className='dropdown-content'>
+            {items
+              .filter((item) => item.value.includes(inputValue))
+              .map(({ value }) => (
+                <button key={value} type='button' className='dropdown-item button is-white'>
+                  {value}
+                </button>
+              ))}
+          </div>
+        </div>
+      </div>
+    )}
+  </Downshift>
 );
 
 export default Patterns;
