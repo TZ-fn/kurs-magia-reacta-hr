212f291bd9f128c5f0372d692e7107867d2fa580
Tomek <tomekz@vp.pl>
2021-03-09 15:52:46 +0100

    Add useState hook to the component to test it

--- src/components/views/Components.jsx
+++ src/components/views/Components.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Components = () => (
-  <div>
-    <h2 className='title is-3'>Components</h2>
-  </div>
-);
+const Components = () => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div>
+      <h2 className='title is-3'>Components</h2>
+      <p className>{counter}</p>
+      <button type='button' className='button' onClick={incrementCounter}>
+        Increment
+      </button>
+    </div>
+  );
+};
 
 export default Components;
