9608ed5b4cb93f64dd4c583f41d88ef3b60a356c
Tomek <tomekz@vp.pl>
2021-03-11 16:12:54 +0100

    Add a notification and some styling

--- src/components/views/Components.jsx
+++ src/components/views/Components.jsx
@@ -1,27 +1,45 @@
 import React, { useState } from 'react';
+import cx from 'classnames';
+import styles from './Components.module.scss';
 
 const Components = () => {
-  const [counter, setCounter] = useState(0);
-  const [isCounterVisible, setIsCounterVisible] = useState(true);
+  const [inputContent, setInputContent] = useState('');
+  const [isInputVisible, setIsInputVisible] = useState(true);
 
-  const incrementCounter = () => {
-    setCounter(counter + 1);
+  const handleInputChange = (e) => {
+    setInputContent(e.target.value);
   };
 
   return (
     <div>
       <h2 className='title is-3'>Components</h2>
-      <button
-        type='button'
-        className='button'
-        onClick={() => setIsCounterVisible(!isCounterVisible)}
-      >
-        Show counter
-      </button>
-      {isCounterVisible ? <p className>{counter}</p> : null}
-      <button type='button' className='button' onClick={incrementCounter}>
-        Increment
-      </button>
+      <div className={styles.wrapper}>
+        {isInputVisible ? (
+          <input
+            name='name'
+            type='text'
+            className={cx('input', 'is-large', styles['margin-bottom'])}
+            onChange={handleInputChange}
+            value={inputContent}
+          />
+        ) : null}
+
+        <button
+          type='button'
+          className={cx('button', 'is-warning', styles['margin-bottom'])}
+          onClick={() => setIsInputVisible(!isInputVisible)}
+        >
+          {isInputVisible ? 'Hide input' : 'Show input'}
+        </button>
+
+        <div className='notification is-primary'>
+          <button type='button' className='delete' aria-label='Close this notification.' />
+          Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor.
+          <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla.
+          Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+          efficitur.
+        </div>
+      </div>
     </div>
   );
 };
