3a6ab7dac27fdafdafe5c521da5b7d2f8e28618e
Tomek <tomekz@vp.pl>
2021-03-12 15:28:59 +0100

    Refactor the Components view to make a To Do list

--- src/components/views/Components.jsx
+++ src/components/views/Components.jsx
@@ -4,7 +4,18 @@ import styles from './Components.module.scss';
 
 const Components = () => {
   const [inputContent, setInputContent] = useState('');
-  const [isInputVisible, setIsInputVisible] = useState(true);
+  const [itemsList, setItemsList] = useState([
+    {
+      id: 0,
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui autem laudantium quidem tenetur placeat beatae tempora sint iusto molestias consectetur magni, assumenda incidunt.',
+    },
+    {
+      id: 1,
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui autem laudantium quidem tenetur placeat beatae tempora sint iusto molestias consectetur magni, assumenda incidunt.',
+    },
+  ]);
 
   const handleInputChange = (e) => {
     setInputContent(e.target.value);
@@ -12,32 +23,34 @@ const Components = () => {
 
   return (
     <div>
-      <h2 className='title is-3'>Components</h2>
-      <div className={styles.wrapper}>
-        {isInputVisible ? (
+      <div className='container'>
+        <div className={styles.wrapper}>
+          <h2 className='title is-3'>To Do List</h2>
           <input
             name='name'
             type='text'
-            className={cx('input', 'is-large', styles['margin-bottom'])}
+            className={cx('input', styles['margin-bottom'])}
             onChange={handleInputChange}
             value={inputContent}
+            placeholder='Write your task...'
           />
-        ) : null}
 
-        <button
-          type='button'
-          className={cx('button', 'is-warning', styles['margin-bottom'])}
-          onClick={() => setIsInputVisible(!isInputVisible)}
-        >
-          {isInputVisible ? 'Hide input' : 'Show input'}
-        </button>
-
-        <div className='notification is-primary'>
-          <button type='button' className='delete' aria-label='Close this notification.' />
-          Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor.
-          <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla.
-          Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
-          efficitur.
+          <button
+            type='button'
+            className={cx('button', 'is-primary', styles['margin-bottom'])}
+            onClick={setItemsList}
+          >
+            Add a new item
+          </button>
+          <h3 className='title is-4'>To do&apos;s:</h3>
+          {itemsList.map((item) => {
+            return (
+              <div className='notification is-background'>
+                <button type='button' className='delete' aria-label='Close this notification.' />
+                {item.content}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
