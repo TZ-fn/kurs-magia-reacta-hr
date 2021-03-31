9132a0824367e6f6772ecb8829354a5348887cd0
Tomek <tomekz@vp.pl>
2021-03-31 15:49:03 +0200

    Add some funcionality to the Controls buttons

--- src/components/MultiStep/MultiStep.jsx
+++ src/components/MultiStep/MultiStep.jsx
@@ -1,32 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react';
+import PropTypes, { element } from 'prop-types';
 
-export const Page = ({ children }) => <div>{children}</div>;
+const WizardContext = React.createContext({
+  currentPage: 1,
+  changePage: () => {},
+});
 
-export const Controls = () => (
-  <div>
-    <button className='button' type='button'>
-      Back
-    </button>
-    <button className='button' type='button'>
-      Next
-    </button>
-    <button className='button' type='button'>
+const Page = ({ children }) => <div>{children}</div>;
+
+const Controls = () => {
+  const context = useContext(WizardContext);
+  const { changePage } = context;
+
+  return (
+    <div>
+      <button onClick={() => changePage(-1)} className='button' type='button'>
+        Back
+      </button>
+      <button onClick={() => changePage(1)} className='button' type='button'>
+        Next
+      </button>
+      {/* <button className='button' type='button'>
       Submit
-    </button>
-  </div>
-);
-
-const MultiStep = ({ children }) => {
-  return <div>{children}</div>;
+    </button> */}
+    </div>
+  );
 };
 
-MultiStep.propTypes = {
-  children: PropTypes.element.isRequired,
+const Wizard = ({ children }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const changePage = (newPageIndex) => {
+    setCurrentPage((oldPageIndex) => oldPageIndex + newPageIndex);
+  };
+
+  return (
+    <WizardContext.Provider value={{ currentPage, changePage }}>
+      {children}
+      <p>{currentPage}</p>
+    </WizardContext.Provider>
+  );
+};
+
+Wizard.propTypes = {
+  children: PropTypes.arrayOf(element).isRequired,
 };
 
 Page.propTypes = {
   children: PropTypes.element.isRequired,
 };
 
-export default MultiStep;
+export { Page, Controls, Wizard };
--- src/components/views/MultiStepView.jsx
+++ src/components/views/MultiStepView.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import MultiStep, { Page, Controls } from '../MultiStep/MultiStep';
+import * as MultiStep from '../MultiStep/MultiStep';
 
 export default function MultiStepView() {
   return (
     <div>
       <h2 className='title is-3'>Compound Components Practice</h2>
-      <MultiStep>
-        <Page>1</Page>
-        <Page>2</Page>
-        <Page>3</Page>
-        <Controls />
-      </MultiStep>
+      <MultiStep.Wizard>
+        <MultiStep.Page>1</MultiStep.Page>
+        <MultiStep.Page>2</MultiStep.Page>
+        <MultiStep.Page>3</MultiStep.Page>
+        <MultiStep.Controls />
+      </MultiStep.Wizard>
     </div>
   );
 }
