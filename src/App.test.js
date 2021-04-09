9ae57c14279a931d8be3df2d0cfe4377cfebed2b
Tomek <tomekz@vp.pl>
2021-04-09 14:37:06 +0200

    Add more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
 import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders', () => {
-  render(<App />);
+it('The App renders default view', () => {
+  const divContainer = document.createElement('div');
+  ReactDOM.render(<App />, divContainer);
+  const appDiv = divContainer.querySelector('.app');
+  expect(appDiv.innerHTML).toContain('Documents');
 });
